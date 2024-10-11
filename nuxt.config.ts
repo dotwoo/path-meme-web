a6ee28e356973d35f541e6dc82e9509b630821c4
dotwoo <dotwoo@gmail.com>
2024-10-11 15:27:49 +0800

    update nuxt 配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,80 +6,85 @@ export default defineNuxtConfig({
   // },
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      titleTemplate: '%s - 古思乱讲',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      titleTemplate: "%s - Dotwoo",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'format-detection', content: 'telephone=no' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "format-detection", content: "telephone=no" },
         // Open Graph
-        { property: 'og:type', content: 'website' },
+        { property: "og:type", content: "website" },
         // Twitter Card
-        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: "twitter:card", content: "summary_large_image" },
       ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ]
-    }
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
   },
   ssr: false,
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@stefanobartoletti/nuxt-social-share', '@nuxtjs/sitemap', '@nuxtjs/supabase', 'nuxt-gtag', 'nuxt-clarity-analytics'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@stefanobartoletti/nuxt-social-share",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/supabase",
+    "nuxt-gtag",
+    "nuxt-clarity-analytics",
+  ],
 
   // configuration options
   socialShare: {
-    baseUrl: 'https://momo.gusibi.mobi' // required!
+    baseUrl: "https://momo.gusibi.mobi", // required!
     // other optional module options
   },
 
-
   tailwindcss: {
     cssPath: ["~/assets/css/tailwind.css", { injectPosition: 0 }],
-    configPath: 'tailwind.config.js',
+    configPath: "tailwind.config.js",
     exposeConfig: false,
     injectPosition: 0,
     viewer: true,
   },
 
   plugins: [
-    '~/plugins/marked.ts',
-    '~/plugins/head.ts',
-    '~/plugins/banner-content.ts',
+    "~/plugins/marked.ts",
+    "~/plugins/head.ts",
+    "~/plugins/banner-content.ts",
   ],
   routeRules: {
     // 指定只有 /profile 路由需要身份验证
-    '/profile': { auth: true },
+    "/profile": { auth: true },
   },
   // 确保中间件被加载
   hooks: {
-    'pages:extend'(pages) {
+    "pages:extend"(pages) {
       pages.forEach((page) => {
-        if (page.path === '/profile') {
-          page.meta = page.meta || {}
-          page.meta.middleware = ['auth']
+        if (page.path === "/profile") {
+          page.meta = page.meta || {};
+          page.meta.middleware = ["auth"];
         }
-      })
-    }
+      });
+    },
   },
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.API_BASE_URL || 'https://path-memo-api.gusibi.mobi',
-      repoOwner: process.env.REPO_OWNER || 'gusibi',
-      repoName: process.env.REPO_NAME || 'path-meme-db',
-      perPageSize: process.env.PER_PAGE_SIZE || '10',
-      siteTitle: '古思乱讲',
-      siteDescription: '古思乱讲 - 一个关于技术、生活和思考的博客',
-      siteUrl: process.env.SITE_URL || 'https://momo.gusibi.mobi',
-      twitterHandle: '@yourtwitterhandle'
+      apiBaseUrl: process.env.API_BASE_URL || "https://blog.dotwoo.us.kg",
+      repoOwner: process.env.REPO_OWNER || "dotwoo",
+      repoName: process.env.REPO_NAME || "path-meme-db",
+      perPageSize: process.env.PER_PAGE_SIZE || "10",
+      siteTitle: "古思乱讲",
+      siteDescription: "古思乱讲 - 一个关于技术、生活和思考的博客",
+      siteUrl: process.env.SITE_URL || "https://blog.dotwoo.us.kg",
+      twitterHandle: "@yourtwitterhandle",
     },
     private: {
-      githubToken: process.env.GITHUB_TOKEN
-    }
+      githubToken: process.env.GITHUB_TOKEN,
+    },
   },
   gtag: {
-    id: 'G-N7EW8Y4SEF'
+    id: "G-N7EW8Y4SEF",
   },
   supabase: {
     // Options
@@ -90,16 +95,12 @@ export default defineNuxtConfig({
   },
   // Sitemap configuration
   sitemap: {
-    // hostname: 'https://momo.gusibi.mobi',
+    // hostname: 'https://blog.dotwoo.us.kg',
     // gzip: true,
-    exclude: [
-      '/admin/**'
-    ],
-    sources: [
-      '/api/__sitemap__/urls'
-    ]
+    exclude: ["/admin/**"],
+    sources: ["/api/__sitemap__/urls"],
   },
   nitro: {
-    preset: 'cloudflare-pages'
+    preset: "cloudflare-pages",
   },
-})
+});
